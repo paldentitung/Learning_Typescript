8f344f5c2d9458037a7efdc1a76c58dae4e74ebc
palden dorje titung <paldendorjetitung@gmail.com>
2026-02-17 16:18:15 +0545

    understand basic type safe

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+let username = "Palden Dorje Titung";
+let age = 19;
+let hobbies = ["Reading", "Coding", "Gaming"];
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+
+
+let username : string = "Palden Dorje Titung";
+let age : number = 19;
+let hobbies : string[]= ["Reading","Coding","Gaming"];
+
+
+console.log("Username",username)
+console.log("Age",age)
+console.log("Hobbies",hobbies)
